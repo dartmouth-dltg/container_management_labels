f867de8abc12c4b3def94f32eeedde810b00da07
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2017-04-04 17:25:39 -0400

    Fix for edge case of no catalog location in resource
    This should *never* happen, but its not a required field, so add an
    additional check to make sure we don;t try to act on a nil object

--- backend/model/label_data.rb
+++ backend/model/label_data.rb
@@ -74,7 +74,7 @@ class LabelData
   
   # returns the catalog location for a top container
   def catalog_location_for_top_container(tc)
-    tc_cat_loc = tc['collection'][0]['_resolved']['user_defined']['enum_1'] ? tc['collection'][0]['_resolved']['user_defined']['enum_1'] : nil
+    tc_cat_loc = tc['collection'][0]['_resolved']['user_defined'] && tc['collection'][0]['_resolved']['user_defined']['enum_1'] ? tc['collection'][0]['_resolved']['user_defined']['enum_1'] : ''
     
     tc_cat_loc
   end
