826780dd42ddf30790b33292535cbc33baee350c
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2021-10-07 15:29:38 -0400

    add additional permitted params

--- backend/plugin_init.rb
+++ backend/plugin_init.rb
@@ -1,6 +1,6 @@
-  # check to see if archival object print levels has been set
-  unless AppConfig.has_key?(:container_management_labels_print_levels)
-    $stderr.puts " WARNING: container management_labels plugin has no print_levels parameter defined. " +
-    "Setting default to print file labels only."
-    AppConfig[:container_management_labels_print_levels] = ["file"]
-  end
+# check to see if archival object print levels has been set
+unless AppConfig.has_key?(:container_management_labels_print_levels)
+  $stderr.puts " WARNING: container management_labels plugin has no print_levels parameter defined. " +
+  "Setting default to print file labels only."
+  AppConfig[:container_management_labels_print_levels] = ["file"]
+end
