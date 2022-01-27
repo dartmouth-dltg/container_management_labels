fb866288e03f56d42ed46ae1c0796c415952cff4
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-01-27 16:58:19 -0500

    fix reload of manage top containers with indicator

--- frontend/controllers/top_containers_labels.rb
+++ frontend/controllers/top_containers_labels.rb
@@ -3,14 +3,14 @@ require 'uri'
 class TopContainersLabelsController < TopContainersController
 
   set_access_control  "view_repository" => [:show, :typeahead, :bulk_operations_browse, :print_labels]
- 
+
   def print_labels
     if params[:print_files]
       post_uri = "/repositories/#{session[:repo_id]}/top_containers_labels/print_sub_labels"
     else
       post_uri = "/repositories/#{session[:repo_id]}/top_containers_labels/print_labels"
     end
-    
+
     response = JSONModel::HTTP.post_form(URI(post_uri), {"record_uris[]" => Array(params[:record_uris])})
 
     results = ASUtils.json_parse(response.body)
@@ -31,30 +31,32 @@ end
 class TopContainersController
 
   private
-  
+
   alias old_perform_search perform_search
 
   def perform_search
     unless params[:indicator].blank?
-      unless params[:q].blank?
+      if params[:q].blank?
+        params[:q] = ""
+      else
         params[:q] = "#{params[:q]} AND "
       end
-      
+
       #convert the range into a set of indicators since indicators are defined as strings and we need exact matches
       if params[:indicator].downcase.include? "to"
         range = params[:indicator].split
           .find_all{|e| e[/\d+/]}
           .each{|e| e.gsub!(/\[|\]/,'').to_i}
-          
+
         indicators = (range[0]..range[range.length-1]).step(1).to_a
       # otherwise just split the list up
       else
         indicators = params[:indicator].split
       end
-      
+
       # then concatenate with the correct prefix and OR the search
       indicator_string = indicators.each { |e| e.prepend('indicator_u_stext:') }.join(" OR ")
-      
+
       params[:q] << indicator_string
     end
 
