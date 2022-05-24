85870def59051f4392c9f0a17bea09ed226d9635
Joshua Shaw <joshua.d.shaw@dartmouth.edu>
2022-05-24 09:35:30 -0400

    routes under prefix

--- frontend/routes.rb
+++ frontend/routes.rb
@@ -1,3 +1,7 @@
 ArchivesSpace::Application.routes.draw do
-  match('/plugins/top_containers_labels/print_labels' => 'top_containers_labels#print_labels', :via => [:post])
+  [AppConfig[:frontend_proxy_prefix], AppConfig[:frontend_prefix]].uniq.each do |prefix|
+    scope prefix do
+      match('plugins/top_containers_labels/print_labels' => 'top_containers_labels#print_labels', :via => [:post])
+    end
+  end
 end
